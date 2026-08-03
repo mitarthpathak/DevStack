3d6c000ac1bca7bffdaba3fda07ad04a943e9129
mitarth pathak <97668137+mitarthpathak@users.noreply.github.com>
2026-08-03 21:26:14 +0530

    Delete src/main/java/com/mitarth/devstack directory

--- src/main/java/com/mitarth/devstack/repo/repo.java
+++
@@ -1,8 +0,0 @@
-package com.mitarth.devstack.repo;
-
-import com.mitarth.devstack.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repo extends JpaRepository<Users, Integer> {
-    Users findByUsername(String username);
-}
